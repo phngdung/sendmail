b1a7f57788b3d412f5328b9f478c44a41125286f
phngdung <phuongdung1720905@gmail.com>
2021-07-22 10:36:14 +0700

    update mail

--- main.go
+++ main.go
@@ -1,69 +1,106 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "log"
-    "net/http"
-    "net/smtp"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/smtp"
+	"os"
 )
 
+type MailServerInfo struct {
+	MailFrom string
+	MailPass string
+	MailHost string
+	MailPort string
+}
+
+var (
+	mail *MailServerInfo
+)
+
+func GetMailOption() *MailServerInfo {
+	if mail != nil {
+		return mail
+	}
+	file, err := os.Open("../config.json")
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	defer file.Close()
+	err = json.NewDecoder(file).Decode(&mail)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	return mail
+}
+
 func sendMail(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "GET" {
-        t, _ := template.ParseFiles("form.gtpl")
-        t.Execute(w, nil)
-    } else {
-        r.ParseForm()
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("template.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
 
-        var email string
-        var title string
-        var message string
-        
-        email= r.FormValue("email")
-        title= r.FormValue("title")
-        message= r.FormValue("message")
+		var email string
+		var title string
+		var message string
 
-        send(email,title,message)
-    }
+		email = r.FormValue("email")
+		title = r.FormValue("title")
+		message = r.FormValue("content")
+
+		result, status := send(email, title, message)
+		if result {
+			fmt.Fprintf(w, "To : %s\n", email)
+			fmt.Fprintf(w, "Email Sent Successfully!")
+		}
+		if !result {
+			fmt.Fprintf(w, "Error: %s!\n", status)
+
+		}
+	}
+
+}
+
+func send(toMail string, subject string, body string) (bool, string) {
+
+	mailConfig := GetMailOption()
+
+	from := mailConfig.MailFrom
+	password := mailConfig.MailPass
+
+	to := []string{
+		toMail,
+	}
+
+	smtpHost := mailConfig.MailHost
+	smtpPort := mailConfig.MailPort
+
+	message := []byte("From: Demo<" + from + ">\r\n" +
+		"Subject:" + subject + "\r\n" +
+		"\r\n" +
+		body)
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		fmt.Println(err)
+		return false, err.Error()
+	}
+	fmt.Println("Email Sent Successfully!")
+	return true, "OK"
 }
 
 func main() {
-    http.HandleFunc("/sendMail", sendMail)
-    err := http.ListenAndServe(":9090", nil) 
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.HandleFunc("/sendMail", sendMail)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
-
-func send(toMail string,subject string, body string) {
-
-    // Sender data.
-    from := "noreply@daugia.io"
-    password := "abcD123$"
-  
-    // Receiver email address.
-    to := []string{
-      toMail,
-    }
-  
-    // smtp server configuration.
-    smtpHost := "smtp.yandex.com"
-    smtpPort := "587"
-  
-    // Message.
-    message := []byte("From: Demo<noreply@daugia.io>\r\n" +
-          "Subject:"+ subject+"\r\n" +
-          "\r\n" +
-           body)
-          
-    // Authentication.
-    auth := smtp.PlainAuth("", from, password, smtpHost)
-    
-    // Sending email.
-    err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-    if err != nil {
-      fmt.Println(err)
-      return
-    }
-    fmt.Println("Email Sent Successfully!")
-  }
